6997e971dbee25129d6e2599639bd8157b4bd4d9
Atter12xd <1485275@senati.pe>
2024-09-16 00:42:29 -0500

    Crear los datos

--- py/mat.py
+++ py/mat.py
@@ -1,3 +1,9 @@
-# Paso 1
+# Paso 1 Importar las Librerías Necesarias
 import matplotlib.pyplot as plt
 import numpy as np
+
+# paso 2 Crear los datos 
+x = np.linspace(-10, 10, 200)  # 200 puntos entre -10 y 10
+y1 = x**2  # y1 = x^2
+y2 = 2 * x + 3  # y2 = 2x + 3
+
